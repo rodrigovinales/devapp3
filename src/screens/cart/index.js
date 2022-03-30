82e3d5a89b31a06757dbce364278e3f5efde7fcc
rodrigo vinales <rodrigo11@live.com.ar>
2022-03-30 19:47:06 -0300

    Clase 14

--- src/screens/cart/index.js
+++ src/screens/cart/index.js
@@ -2,3 +2,5 @@ import React, { useEffect, useState } from 'react'
 import { styles } from './styles'
 import { View, Text, StyleSheet, FlatList } from "react-native";
 
+import { connect, useSelector } from 'react-redux';
+
